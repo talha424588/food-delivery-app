1985ad78864b50847b7241fee03bcd53bc9ee0d6
Muhammad Talha <talha424588@gmail.com>
2024-04-16 01:47:15 +0500

    nav icon added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
+
+
 function Navbar()
 {
     return (
         <>
-        <h1>Navbar</h1>
+        <nav>
+            <a href="/"><AiOutlineHome /></a>
+            <a href="about"><AiOutlineUser /></a>
+            <a href="experience"><BiBook /></a>
+            <a href="services"><RiServiceLine /></a>
+            <a href="contact"><BiMessageSquareDetail /></a>
+        </nav>
         </>
     )
 }
