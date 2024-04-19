50fa5c9a42e2f7a4ed8d2f771750d2591f0337d2
Muhammad Talha <talha424588@gmail.com>
2024-04-19 11:20:01 +0500

    experience portion complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,46 +2,52 @@ import Me from "../../assets/me-about.jpg";
 import { FaAward } from "react-icons/fa6";
 import { LuUsers } from "react-icons/lu";
 import { RiFolderChartLine } from "react-icons/ri";
-import "../about/about.css"
-function About()
-{
-    return (
-        <>
-        <section id="about">
-            <h5>Get To Know</h5>
-            <h2>About Me</h2>
-            <div className="container about_container">
-                <div className="about_me">
-                        <div className="about_me-image">
-                            <img src={Me} alt="" />
-                    </div>
-                </div>
-                <div className="about__content">
-                    <div className="about__cards">
-                     <article className="about__card">
-                        <FaAward className="about__icon" />
-                        <h5>Experience</h5>
-                        <small>3+ Years Working</small>
-                     </article>
-
-                     <article className="about__card">
-                        <LuUsers className="about__icon" />
-                        <h5>Clients</h5>
-                        <small>200+ Worldwide</small>
-                     </article>
-
-                     <article className="about__card">
-                        <RiFolderChartLine className="about__icon" />
-                        <h5>Projects</h5>
-                        <small>80+ Completed</small>
-                     </article>
-                    </div>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum optio necessitatibus, debitis aspernatur inventore eveniet enim iusto eaque quia ut. Itaque fugit illum dolores repellendus a consectetur blanditiis, iure reprehenderit.</p>
-                    <a href="#contact" className="btn btn-primary">Let's Talk</a>
-                </div>
+import "../about/about.css";
+function About() {
+  return (
+    <>
+      <section id="about">
+        <h5>Get To Know</h5>
+        <h2>About Me</h2>
+        <div className="container about_container">
+          <div className="about_me">
+            <div className="about_me-image">
+              <img src={Me} alt="" />
             </div>
-        </section>
-        </>
-    )
+          </div>
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FaAward className="about__icon" />
+                <h5>Experience</h5>
+                <small>3+ Years Working</small>
+              </article>
+
+              <article className="about__card">
+                <LuUsers className="about__icon" />
+                <h5>Clients</h5>
+                <small>200+ Worldwide</small>
+              </article>
+
+              <article className="about__card">
+                <RiFolderChartLine className="about__icon" />
+                <h5>Projects</h5>
+                <small>80+ Completed</small>
+              </article>
+            </div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum
+              optio necessitatibus, debitis aspernatur inventore eveniet enim
+              iusto eaque quia ut. Itaque fugit illum dolores repellendus a
+              consectetur blanditiis, iure reprehenderit.
+            </p>
+            <a href="#contact" className="btn btn-primary">
+              Let's Talk
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default About;
--- src/components/experience/Expericence.jsx
+++ src/components/experience/Expericence.jsx
@@ -1,9 +1,111 @@
-function Experience()
-{
-    return (
-        <>
-        <section id="experience">Expericen</section>
-        </>
-    )
+import { BsFillPatchCheckFill } from "react-icons/bs";
+import "../experience/experience.css";
+function Experience() {
+  return (
+    <>
+      <section id="experience">
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+        <div className="container experience_container">
+          <div className="experience_frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience_content">
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>HTML</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>CSS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>JavaScript</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>Bootstrap</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>Tailwind</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>React</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+            </div>
+          </div>
+          <div className="experience_backend">
+            <h3>Backend Development</h3>
+            <div className="experience_content">
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>Node JS</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className='experience_details_icon' />
+                <div>
+                  <h4>MongoDB</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>PHP</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>MySQL</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+
+              <article className="experience_details">
+                <BsFillPatchCheckFill className="experience_details_icon" />
+                <div>
+                  <h4>Python</h4>
+                  <small className="text-light">Experienced</small>
+                </div>
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 export default Experience;
