42f2bf5bc47b0043e34fdbdb8b5f8f73f4c925d2
Muhammad Talha <talha424588@gmail.com>
2024-04-19 15:38:35 +0500

    service section complete

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -1,9 +1,164 @@
-function Service()
-{
-    return (
-        <>
-        <section id="services">Service</section>
-        </>
-    )
+import { BiCheck } from "react-icons/bi";
+import '../service/service.css';
+function Service() {
+  return (
+    <>
+      <section id="services">
+        <h5>What I Offer</h5>
+        <h2>Services</h2>
+        <div className="container services_container">
+          <article className="service">
+            <div className="service_head">
+              <h3>UI/UX Design</h3>
+            </div>
+            <ul className="service_list">
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+            </ul>
+          </article>
+
+          <article className="service">
+            <div className="service_head">
+              <h3>Web Development</h3>
+            </div>
+            <ul className="service_list">
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+            </ul>
+          </article>
+
+          <article className="service">
+            <div className="service_head">
+              <h3>Content Creation</h3>
+            </div>
+            <ul className="service_list">
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+              <li>
+                <BiCheck className="service_list_icon" />
+                <p className="service_list_text">
+                  I can help you with the design of your website
+                </p>
+              </li>
+            </ul>
+          </article>
+        </div>
+      </section>
+    </>
+  );
 }
 export default Service;
