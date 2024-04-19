feee5d07fc317dd20e91c3ed0136123b9e632edf
Muhammad Talha <talha424588@gmail.com>
2024-04-19 06:33:25 +0500

    about section completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { BiBook } from "react-icons/bi";
 import { RiServiceLine } from "react-icons/ri";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import '../Navbar/Navbar.css'
+import { useState } from "react";
 
 function Navbar()
 {
+    const [activeNav, setActiveNav] = useState("#");
     return (
         <>
         <nav>
-            <a href="#" className="active"><AiOutlineHome /></a>
-            <a href="#about"><AiOutlineUser /></a>
-            <a href="#experience"><BiBook /></a>
-            <a href="#services"><RiServiceLine /></a>
-            <a href="#contact"><BiMessageSquareDetail /></a>
+            <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}><AiOutlineHome /></a>
+            <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><AiOutlineUser /></a>
+            <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBook /></a>
+            <a href="#services" onClick={() =>  setActiveNav("#services")} className={activeNav === "#services" ? "active" : ""}><RiServiceLine /></a>
+            <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><BiMessageSquareDetail /></a>
         </nav>
         </>
     )
